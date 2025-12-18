4c1ac1f1a269eb2c8a337915512701ea0a81e530
Ajay <ajaykumar20031031@gmail.com>
2025-12-18 06:38:15 +0530

    welcome email impolemented

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,8 +1,11 @@
 import mongoose from "mongoose"
+import { ENV } from "./env.js";
 
 export const connectDB = async () => {
   try {
-    const con=await mongoose.connect(process.env.MONGO_URI)
+    const { MONGO_URI } = ENV;
+    if(!MONGO_URI)throw new Error("mONGO_URI is not set")
+    const con=await mongoose.connect(ENV.MONGO_URI)
     console.log("MONGODB Connected", con.connection.host)
   } catch (error) {
     console.error("Error connecting to MONGODB", error)
--- backend/src/server.js
+++ backend/src/server.js
@@ -4,10 +4,11 @@ import authRoutes from "./routes/auth.route.js";
 import messageRouter from "./routes/message.route.js";
 import path from "path";
 import { connectDB } from "./lib/db.js";
+import { ENV } from "./lib/env.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = ENV.PORT || 3000;
 const app = express();
 const __dirname = path.resolve();
 
@@ -16,13 +17,13 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRouter);
 
 //make ready for deployment
-if (!process.env.NODE_ENV == "production") {
+if (!ENV.NODE_ENV == "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
   app.get("*", (_, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
-app.listen(PORT, () => {
+app.listen(ENV.PORT, () => {
   console.log("Server running at port", PORT)
   connectDB()
 })
