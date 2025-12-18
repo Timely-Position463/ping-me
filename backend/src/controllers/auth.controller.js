f6db25ed3eb6ded04860c1dced7eb38b6bbefed8
Ajay <ajaykumar20031031@gmail.com>
2025-12-18 07:23:18 +0530

    login& logout functionality added

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -68,6 +68,10 @@ export const signup = async (req, res) => {
 export const login = async (req, res) => {
   const { email, password } = req.body;
   try {
+    if (!email || !password)
+      return res
+        .status(400)
+        .json({ message: "Email and password are required" });
     const user = await User.findOne({ email });
     if (!user) return res.status(400).json({ message: "Invalid credentials" });
     const isPasswordCorrect = await bcrypt.compare(password, user.password);
