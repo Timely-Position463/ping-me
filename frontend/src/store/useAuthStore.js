9c6ab86d90964b3f32ce3f6dddf56bdb0e157757
Ajay <ajaykumar20031031@gmail.com>
2025-12-18 22:18:23 +0530

    login & logout implemented

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,34 +1,58 @@
-import { create } from "zustand"
+import { create } from "zustand";
 import { api } from "../lib/axios";
-import {toast} from "react-hot-toast"
+import { toast } from "react-hot-toast";
 
 export const useAuthStore = create((set) => ({
   authUser: null,
   isCheckingAuth: true,
-  isSigningUp:false,
-  
+  isSigningUp: false,
+  isLoggingIn: false,
+
   checkAuth: async () => {
     try {
-      const res = await api.get("/auth/check")
+      const res = await api.get("/auth/check");
       set({ authUser: res.data });
     } catch (error) {
-      console.log("Error in authCheck", error)
-      set({authUser:null})
+      console.log("Error in authCheck", error);
+      set({ authUser: null });
     } finally {
-      set({isCheckingAuth:false})
+      set({ isCheckingAuth: false });
     }
   },
 
   signup: async (data) => {
-    set({ isSigningUp: true })
+    set({ isSigningUp: true });
     try {
       const res = await api.post("/auth/signup", data);
-      set({ authUser: res.data })
-      toast.success("Account created successfully")
+      set({ authUser: res.data });
+      toast.success("Account created successfully");
     } catch (error) {
-      toast.error(error.response.data.message)
+      toast.error(error.response.data.message);
     } finally {
-      set({isSigningUp:false})
+      set({ isSigningUp: false });
+    }
+  },
+  login: async (data) => {
+    set({ isLoggingIn: true });
+    try {
+      const res = await api.post("/auth/login", data);
+      set({ authUser: res.data });
+      toast.success("Logged in successfully");
+    } catch (error) {
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isLoggingIn: false });
+    }
+  },
+
+  logout: async() => {
+    try {
+      await api.post("/auth/logout");
+      set({authUser:null})
+      toast.success("Logged out successfully")
+    } catch (error) {
+      toast.error("Error logging out")
+      console.log("Logout error",error)
     }
   }
-}))
+}));
