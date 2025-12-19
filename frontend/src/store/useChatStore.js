a3d187ffa5416a4260c78ae1a2a0a70152c8666f
Ajay <ajaykumar20031031@gmail.com>
2025-12-19 19:10:12 +0530

    send message func. implemented

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -1,6 +1,7 @@
 import { create } from "zustand";
 import { api } from "../lib/axios.js";
 import { toast } from "react-hot-toast";
+import { useAuthStore } from "../store/useAuthStore.js"
 
 export const useChatStore = create((set, get) => ({
   allContacts: [],
@@ -11,7 +12,7 @@ export const useChatStore = create((set, get) => ({
   isUserLoading: false,
   isMessagesLoading: false,
   isSoundEnabled: JSON.parse(localStorage.getItem("isSoundEnabled")) === true,
-  
+
   setActiveTab: (tab) => set({ activeTab: tab }),
   setSelectedUser: (selectedUser) => set({ selectedUser }),
 
@@ -45,15 +46,45 @@ export const useChatStore = create((set, get) => ({
   },
 
   getMessagesByUserId: async (userId) => {
-    set({ isMessagesLoading: true })
+    set({ isMessagesLoading: true });
     try {
-      const res = await api.get(`/messages/${userId}`)
-      set({ messages: res.data })
+      const res = await api.get(`/messages/${userId}`);
+      set({ messages: res.data });
     } catch (error) {
-      toast.error(error.response?.data?.message || "Something went wrong ")
+      toast.error(error.response?.data?.message || "Something went wrong ");
     } finally {
-      set({isMessagesLoading:false})
+      set({ isMessagesLoading: false });
     }
-  }
+  },
 
+  sendMessage: async (messageData) => {
+    const { selectedUser, messages } = get();
+    const { authUser } = useAuthStore.getState();
+
+    const tempId = `temp-${Date.now()}`;
+
+    const optimisticMessage = {
+      _id: tempId,
+      senderId: authUser._id,
+      receiverId: selectedUser._id,
+      text: messageData.text,
+      image: messageData.image,
+      createdAt: new Date().toISOString(),
+      isOptimistic: true, // flag to identify optimistic messages (optional)
+    };
+    // immidetaly update the ui by adding the message
+    set({ messages: [...messages, optimisticMessage] });
+
+    try {
+      const res = await api.post(
+        `/messages/send/${selectedUser._id}`,
+        messageData
+      );
+      set({ messages: messages.concat(res.data) });
+    } catch (error) {
+      // remove optimistic message on failure
+      set({ messages: messages });
+      toast.error(error.response?.data?.message || "Something went wrong");
+    }
+  },
 }));
